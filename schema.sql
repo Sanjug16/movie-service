6cc72beb7b719f30f998845712775b04f17512fb
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-09 16:49:15 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE movie_entity (
+CREATE TABLE movie_details (
     movie_id INT AUTO_INCREMENT PRIMARY KEY,
     movie_title VARCHAR(255) NOT NULL,
     movie_language VARCHAR(255) NOT NULL,
