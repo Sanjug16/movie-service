99d4dcbcad31fd75b7a815bae42a8e21fcb67a05
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-09 17:05:27 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-INSERT INTO movie_entity (movie_title, movie_language, movie_release_date, movie_image_url) VALUES
+INSERT INTO movie_details (movie_title, movie_language, movie_release_date, movie_image_url) VALUES
 ('Inception', 'English', '2010-07-16', 'https://example.com/images/inception.jpg'),
 ('Parasite', 'Korean', '2019-05-30', 'https://example.com/images/parasite.jpg'),
 ('The Dark Knight', 'English', '2008-07-18', 'https://example.com/images/dark_knight.jpg'),
