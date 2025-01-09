04185c8be3f643f2f7c863c59835771a1c0f7535
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-09 15:04:38 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE movie_entity (
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_title VARCHAR(255) NOT NULL,
+    movie_language VARCHAR(255) NOT NULL,
+    movie_release_date DATE NOT NULL,
+    movie_image_url VARCHAR(255)
+);
