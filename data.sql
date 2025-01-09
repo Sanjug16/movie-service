a68281c8c79b9fe430c8fc9bd68e799ccfd754d4
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-09 15:06:52 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO movie_entity (movie_title, movie_language, movie_release_date, movie_image_url) VALUES
+('Inception', 'English', '2010-07-16', 'https://example.com/images/inception.jpg'),
+('Parasite', 'Korean', '2019-05-30', 'https://example.com/images/parasite.jpg'),
+('The Dark Knight', 'English', '2008-07-18', 'https://example.com/images/dark_knight.jpg'),
+('Spirited Away', 'Japanese', '2001-07-20', 'https://example.com/images/spirited_away.jpg'),
+('La La Land', 'English', '2016-12-09', 'https://example.com/images/la_la_land.jpg');
